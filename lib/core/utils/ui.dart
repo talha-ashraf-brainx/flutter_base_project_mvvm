a193b6721545dd3d20acb8c7536e53fd00c44c32
Talha Ashraf <talha.ashraf@brainxtech.com>
2025-03-27 22:57:49 +0500

    minor fix

--- lib/core/utils/ui.dart
+++ lib/core/utils/ui.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 dynamic showModalSheet({
   required BuildContext context,
